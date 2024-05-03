39bc90d96f8c70965db517b4b81d0559d0e8bac4
Litesword <136891668+Litesword@users.noreply.github.com>
2024-05-03 17:21:19 +0530

    delete and view functionality is added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,6 +8,8 @@ function MyForm() {
     email: '',
     phone: ''
   });
+  const [mobile, setmobile] = useState('')
+  const [number, setnumber] = useState('')
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -39,9 +41,41 @@ function MyForm() {
       console.error('Error occurred while submitting the form:', error);
     }
   };
+  const Change = (event) =>{
+    
+    setmobile(event.target.value)
+  }
+
+  const Remove = (event) =>{
+  
+    setnumber(event.target.value)
+  }
+
+  const get_details = async (e) => {
+    e.preventDefault();
+    const result = await axios.get(`http://localhost:8080/api/registrate/${mobile}`);
+  console.log(result.data);
+  setFormData({
+    name: result.data.userName,
+    email: result.data.email,
+    phone: result.data.mobilePhone
+  });
+  if(!result.data){
+    console.log("no such record exist with given phone number")
+  }
+  }
+
+  const del = async (e) => {
+    e.preventDefault();
+    const result =await axios.delete(`http://localhost:8080/api/registrate/${mobile}`);
+  console.log(result);
+  }
 
   return (
+    <>
+    
     <form className="my-form" onSubmit={handleSubmit}>
+    <h2>create user</h2>
       <div className="form-group">
         <label htmlFor="name">Name:</label>
         <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
@@ -56,6 +90,23 @@ function MyForm() {
       </div>
       <button type="submit">Submit</button>
     </form>
+    <form className="my-form" onSubmit={get_details}>
+    <h2>view user</h2>
+      <div className="form-group">
+        <label htmlFor="phone">Phone:</label>
+        <input id="phone" name="phone" value={mobile} onChange={Change}></input>
+      </div>
+      <button type="submit">GET</button>
+    </form>
+    <form className="my-form" onSubmit={del}>
+    <h2>delete user</h2>
+      <div className="form-group">
+        <label htmlFor="phone">Phone:</label>
+        <input id="phone" name="phone" value={number} onChange={Remove}></input>
+      </div>
+      <button type="submit">Delete</button>
+    </form>
+    </>
   );
 }
 
