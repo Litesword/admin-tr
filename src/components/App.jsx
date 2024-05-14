ccc4bc2b4850831f1d606d6ff9d5df801b3a98eb
Litesword <136891668+Litesword@users.noreply.github.com>
2024-05-14 15:51:09 +0530

    removed-unnecessary-components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import MyForm from './Form';
-import AddDepartment from './AddDepartment';
+
 
 function App() {
   const [selectedButton, setSelectedButton] = useState(null);
